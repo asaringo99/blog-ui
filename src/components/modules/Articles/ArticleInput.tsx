3d077120906a73d894f3e1ee9b81c81c9dc55fce
asaringo99 <ka99code@gmail.com>
2023-08-29 00:48:17 +0900

    articles

---
+++ src/components/modules/Articles/ArticleInput.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/base/Button"
+import { ArticleInputContainer } from "./Styles"
+
+
+export const ArticleInput = () => {
+  return (
+    <ArticleInputContainer>
+      <Button label="Create Article" primary="darkblue"/>
+    </ArticleInputContainer>
+  )
+}

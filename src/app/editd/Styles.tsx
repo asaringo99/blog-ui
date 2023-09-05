cdb8666f7b67f4972b82b35bd7656d798a901672
asaringo99 <ka99code@gmail.com>
2023-09-05 22:40:09 +0900

    page update

--- src/app/editd/Styles.tsx
+++
@@ -1,8 +0,0 @@
-import { styled } from "styled-components"
-
-export const PageContainer = styled.div`
-  padding: 50px;
-  display: flex;
-  justify-content: center;
-  
-`;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,6 @@ import Home from "./home/page";
 
 export default function Root() {
   return (
-    <div>
-			<Home/>
-    </div>
+		<Home/>
   )
 }

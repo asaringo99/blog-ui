f3daf9e5ba731b44c4f2791a8cc46b36d0e9759f
asaringo99 <ka99code@gmail.com>
2023-09-07 21:15:28 +0900

    fixed page

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -1,28 +1,21 @@
 "use client"
+import * as React from "react";
 import { Button } from "@/components/base/Button";
 import TextField from "@/components/base/Textfield";
 import WrapperContainer from "@/components/base/Wrapper";
-import { Header } from "@/components/layouts/Header/Header";
 import MarkdownEditor from "@/components/organism/Markdown/Markdown";
-import Link from "next/link";
-import React, { use, useState } from "react";
-import { InputButtonContainer } from "../home/Styles";
+import Template from "../Template";
 
-export default function Create(){
-	const onLogin = () => {}
-	const onLogout = () => {}
-	const onCreateAccount = () => {}
-  
+export default function Create(){  
   return (
-    <div className="">
-      <Header onLogin={onLogin} onLogout={onLogout} onCreateAccount={onCreateAccount}/>
+    <Template>
       <WrapperContainer>
-        <TextField label="Title"></TextField>
+        <TextField fontSize="large" label="Title"/>
       </WrapperContainer>
       <MarkdownEditor/>
       <WrapperContainer>
         <Button label="Create" primary="black" size="large"/>
       </WrapperContainer>
-    </div>
+    </Template>
   )
 }
--- src/app/home/Styles.tsx
+++ src/app/home/Styles.tsx
@@ -1,8 +1,5 @@
 import { styled } from "styled-components"
 
-export const PageContainer = styled.div`
-  height: 100vh;
-`;
 export const InputButtonContainer = styled.div`
   padding: 20px 15px;
 `;
